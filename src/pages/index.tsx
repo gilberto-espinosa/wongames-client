fe675cfa632ad593de9339a67199b747a9975b38
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-09-08 21:24:22 -0300

    Feat - initial apollo file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import Home, { HomeTemplateProps } from '../templates/Home'
+import { gql, useQuery } from '@apollo/client'
+
 import bannersMock from '../components/BannerSlider/mock'
 import gamesMock from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
+import Home, { HomeTemplateProps } from '../templates/Home'
 
 export default function Index(props: HomeTemplateProps) {
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        data {
+          attributes {
+            name
+          }
+        }
+      }
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+
+  if (error) return <p>{error}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
+
   return <Home {...props} />
 }
 
